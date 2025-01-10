ff9c95c478c019c90bf4ff3975ffd68c30980d33
random <vmstankevich@gmail.com>
2025-01-10 14:54:08 +0100

    Eckel chapter 5, task 1,2

---
+++ src/pl/alivin/unpackingjava/examples/eckel/chapter5/task1/Test.java
@@ -0,0 +1,10 @@
+package pl.alivin.unpackingjava.examples.eckel.chapter5.task1;
+
+public class Test {
+    String string;
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        System.out.println(t.string);
+    }
+}
